ab5c26aee469e84dc355f5054746e9037887b954
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-25 12:51:19 +0530

    Login Logic

--- app/src/main/java/com/sveri/firebaseauthapp/LoginActivity.java
+++ app/src/main/java/com/sveri/firebaseauthapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.sveri.firebaseauthapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,7 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -29,6 +34,24 @@ public class LoginActivity extends AppCompatActivity {
         initialiseWidgets();
 
         mAuth = FirebaseAuth.getInstance();
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                etEmail.setText(null);
+                etPwd.setText(null);
+                etPwd.requestFocus();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginUser();
+            }
+        });
         tvRegLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +59,46 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
             }
         });
+    }
+
+    private void loginUser() {
+
+        String email = etEmail.getText().toString().trim();
+        String pwd = etPwd.getText().toString().trim();
+
+        if (email.isEmpty()){
+            etEmail.setError("Email cannot be empty");
+            return;
+        }
+        if (pwd.isEmpty()){
+            etPwd.setError("Password cannot be empty");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this, "Successfull",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }else {
+
+                            Toast.makeText(LoginActivity.this, "Failed "+task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        etEmail.setText(null);
+                        etPwd.setText(null);
+                        etEmail.requestFocus();
+                    }
+
+
+                });
+
     }
 
     public void initialiseWidgets(){
