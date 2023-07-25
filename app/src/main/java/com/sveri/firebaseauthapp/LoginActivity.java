94592239f63172a9970c16aa3d4631c1b7815e1f
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-25 12:28:38 +0530

    Registration Code

--- app/src/main/java/com/sveri/firebaseauthapp/LoginActivity.java
+++ app/src/main/java/com/sveri/firebaseauthapp/LoginActivity.java
@@ -2,13 +2,49 @@ package com.sveri.firebaseauthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tvRegLink;
+    EditText etEmail, etPwd;
+    Button btnSubmit,btnCancel;
+    ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initialiseWidgets();
+
+        mAuth = FirebaseAuth.getInstance();
+        tvRegLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+
+    public void initialiseWidgets(){
+
+        etEmail = findViewById(R.id.editTextEmailAddress);
+        etPwd = findViewById(R.id.editTextPassword);
+        tvRegLink = findViewById(R.id.textViewRegisterLink);
+        btnSubmit = findViewById(R.id.buttonSubmit);
+        btnCancel = findViewById(R.id.buttonCancel);
+        progressBar = findViewById(R.id.progressBarLogin);
     }
 }
--- app/src/main/java/com/sveri/firebaseauthapp/RegistrationActivity.java
+++ app/src/main/java/com/sveri/firebaseauthapp/RegistrationActivity.java
@@ -1,14 +1,110 @@
 package com.sveri.firebaseauthapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    TextView tvLoginLink;
+    EditText etEmail, etPwd;
+    Button btnSubmit,btnCancel;
+    ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        initialiseWidgets();
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                registerNewUser();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                etEmail.setText(null);
+                etPwd.setText(null);
+                etPwd.requestFocus();
+            }
+        });
+
+        tvLoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    public void initialiseWidgets(){
+
+        etEmail = findViewById(R.id.editTextRegEmailAddress);
+        etPwd = findViewById(R.id.editTextRegPassword);
+        tvLoginLink = findViewById(R.id.textViewLoginLink);
+        btnSubmit = findViewById(R.id.buttonRegisster);
+        btnCancel = findViewById(R.id.buttonRegCancel);
+        progressBar = findViewById(R.id.progressBarRegister);
+    }
+
+    public void registerNewUser(){
+
+        String email = etEmail.getText().toString().trim();
+        String pwd = etPwd.getText().toString().trim();
+
+        if (email.isEmpty()){
+            etEmail.setError("Email cannot be empty");
+            return;
+        }
+        if (pwd.isEmpty()){
+            etPwd.setError("Password cannot be empty");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,pwd)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            progressBar.setVisibility(View.GONE);
+
+                            startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                        }else{
+
+                            Toast.makeText(RegistrationActivity.this, "Registeration Failed "+task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                        etEmail.setText(null);
+                        etPwd.setText(null);
+                        etPwd.requestFocus();
+                    }
+
+                });
     }
 }
